035df64c52e9bbd11ace6c8adae0fb90cee6497d
Valerii Besklubyi <marek.mnishek@gmail.com>
2025-04-18 00:11:20 +0200

    build#3

--- vocs.config.ts
+++ vocs.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'vocs'
 
 export default defineConfig({
   rootDir: '.',
-  baseUrl: 'https://promokit.github.io', 
-  basePath: '/alysum-docs',
-  vite: {
-    base: '/alysum-docs',
-  },
   title: 'Alysum',
   sidebar: [
     {
